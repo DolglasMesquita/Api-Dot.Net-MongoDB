99192097fba115dccacab95f57cc49dfeadea476
Dolglas Mesquita <dolglas_mesquita@hotmail.com>
2021-01-10 21:18:33 -0300

    Projeto concluído

---
+++ ApiMongo/Data/Collections/Infectado.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver.GeoJsonObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMongo.Data.Collections
+{
+    public class Infectado
+    {
+        public  DateTime DataNascimento{ get; set; }
+        public string Sexo { get; set; }
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+
+        public Infectado(DateTime dataNascimento, string sexo, double latitude, double longitude)
+        {
+            DataNascimento = dataNascimento;
+            Sexo = sexo;
+            Localizacao = new GeoJson2DGeographicCoordinates( longitude, latitude);
+        }
+    }
+}
